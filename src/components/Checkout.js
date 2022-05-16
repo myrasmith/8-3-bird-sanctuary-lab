21a081caebba70de6c8c232ff7adb4a81744d144
Myra Smith <myra.fowlkes.smith@gmail.com>
2022-05-16 15:29:35 -0400

    small changes

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,20 +1,24 @@
-function Checkout() {
-  return (
-    <aside className="aside checkout">
-      <h2>Checkout</h2>
-      <form>
-        <label htmlFor="firstName">First Name</label>
-        <input type="text" name="firstName" id="firstName" />
-        <label htmlFor="lastName">Last Name</label>
-        <input type="text" name="lastName" id="lastName" />
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="zip">Zip Code</label>
-        <input type="number" name="zip" id="zip" />
-        <input type="submit" value="Submit" />
-      </form>
-    </aside>
-  );
+import { Component } from "react";
+
+class Checkout extends Component {
+  render() {
+    return (
+      <aside className="aside checkout">
+        <h2>Checkout</h2>
+        <form>
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" name="firstName" id="firstName" />
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" name="lastName" id="lastName" />
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" />
+          <label htmlFor="zip">Zip Code</label>
+          <input type="number" name="zip" id="zip" />
+          <input type="submit" value="Submit" />
+        </form>
+      </aside>
+    );
+  }
 }
 
 export default Checkout;
