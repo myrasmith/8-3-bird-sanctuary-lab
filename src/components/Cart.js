72fe016a066b32092dc0f32fd5bf55a603a7962b
Myra Smith <myra.fowlkes.smith@gmail.com>
2022-05-16 15:43:08 -0400

    remove conditional rendering (implemented later)

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -8,12 +8,7 @@ function Cart({ discount, total, bonusItems }) {
       <h4>Total: ${total}</h4>
       <ol class="cart-list Cart"></ol>
       <p>Your donations qualify you for the following items:</p>
-      <ul>
-        {total >= 100 && <li>{bonusItems[0]}</li>}
-        {total >= 300 && <li>{bonusItems[1]}</li>}
-        {total >= 500 && <li>{bonusItems[2]}</li>}
-        {total >= 1000 && <li>{bonusItems[3]}</li>}
-      </ul>
+      <ul></ul>
     </aside>
   );
 }
