c5505c58838c08bfb932fc1016512ec057e204ea
Myra Smith <myra.fowlkes.smith@gmail.com>
2022-05-16 15:42:27 -0400

    adds state and one-directional data flow

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,19 +1,35 @@
 import { Component } from "react";
 
 class Checkout extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      zip: "",
+    };
+  }
+
   render() {
+    const { firstName, lastName, email, zip } = this.state;
     return (
       <aside className="aside checkout">
         <h2>Checkout</h2>
         <form>
           <label htmlFor="firstName">First Name</label>
-          <input type="text" name="firstName" id="firstName" />
+          <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            value={firstName}
+          />
           <label htmlFor="lastName">Last Name</label>
-          <input type="text" name="lastName" id="lastName" />
+          <input type="text" name="lastName" id="lastName" value={lastName} />
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <input type="email" name="email" id="email" value={email} />
           <label htmlFor="zip">Zip Code</label>
-          <input type="number" name="zip" id="zip" />
+          <input type="number" name="zip" id="zip" value={zip} />
           <input type="submit" value="Submit" />
         </form>
       </aside>
