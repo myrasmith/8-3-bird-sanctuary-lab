5a157d15457df4a3eb6bff596dff01017da9825b
Myra Smith <myra.fowlkes.smith@gmail.com>
2022-05-16 12:10:27 -0400

    static app complete

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import BIRDS from "./data/birds";
+import BONUSITEMS from "./data/bonusItems";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App birds={BIRDS} bonusItems={BONUSITEMS} />
   </React.StrictMode>,
   document.getElementById("root")
 );
